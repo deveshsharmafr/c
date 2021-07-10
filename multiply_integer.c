0390d22fa4b9369a344e92ab37ad7e25020632d4
deveshsharmafr <87217740+deveshsharmafr@users.noreply.github.com>
2021-07-10 06:33:18 +0200

    multiply two integers
    C program to multiply 2 integers

---
+++ multiply_integer.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+	int a=0,b=0,mul=0;
+
+        printf("Enter  first integer:");
+        scanf("%d", &a);
+        printf("Enter  second integer:");
+        scanf("%d", &b);
+        mul = a*b;
+        printf("Multiply = %d \n", mul);
+	
+}
