cd1a7c9c8a404b5438c58ac64a3c3c9bbf8fa4ae
deveshsharmafr <87217740+deveshsharmafr@users.noreply.github.com>
2021-07-10 07:43:26 +0200

    area of square
    C program to find the area of square.

---
+++ area_of_square.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main ()
+{
+        system("clear");
+        int a,sq;
+        printf("Enter the side length:");
+        scanf("%d", &a);
+        sq = a*4;
+        printf("Area of this square is %d \n", sq);
+
+        return 0;
+
+}
+
