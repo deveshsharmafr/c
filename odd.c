c68262fab7ffa8be2b08a332bd957c1a4a8f2c4b
deveshsharmafr <87217740+deveshsharmafr@users.noreply.github.com>
2021-07-10 21:54:50 +0200

    odd.
    C program to find odd numbers start limit entered by the user and the end limit entered by the user.

---
+++ odd.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main ()
+{
+	int s,e; 
+	printf("Enter your start limit:-");
+	scanf("%d", &s);
+	printf("Enter your end limit:-");
+	scanf("%d", &e);
+	if()
+    for(int i=s;i<=e;i++)	
+	{
+		if(i%2!=0)
+		{
+				printf("%d\n", i);
+		}
+	}
+	return(0);
+}
+
