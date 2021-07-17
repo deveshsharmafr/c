777a2cb697a97f76f27bc1c9960eae7a6ba01d2a
deveshsharmafr <87217740+deveshsharmafr@users.noreply.github.com>
2021-07-17 07:14:51 +0200

    Update palindrome.c

--- palindrome.c
+++ palindrome.c
@@ -1,17 +1,17 @@
 #include<stdio.h>  
 int main()    
 {    
-int n=0,r=0,sum=0,temp=0;    
+int n=0,r=0,rev=0,t=0;    
 printf("Enter the number:");    
 scanf("%d",&n);    
-temp=n;    
+t=n;    
 while(n!=0)    
 {    
 	r=n%10;    
-	sum=(sum*10)+r;    
+	rev=(rev*10)+r;    
 	n=n/10;    
 }    
-if(temp==sum)    
+if(t==rev)    
 	printf("This number is palindrome\n");    
 else    
 	printf("This number is not palindrome\n"); 
